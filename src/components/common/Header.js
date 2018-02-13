00af0285d93c53a499cf77e34470c164d78baf26
Ante Vuletić Antić <vuletic23@gmail.com>
2018-02-13 12:05:43 +0100

    mockup header buttons

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import FloatingActionButton from "./FloatingActionButton";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div style={{marginBottom: 30}}>
+    <FloatingActionButton />
+    <hr/>
+  </div>;
 };
 
 export default Header;
